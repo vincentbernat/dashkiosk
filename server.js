83b95e8d403182120dff35301d3cea7b6c39c1f5
Vincent Bernat <vbe@deezer.com>
2014-03-27 18:51:51 +0100

    database: first try at using a NoSQL embedded database
    We use nedb which has an API not unlike MongoDB. However, the drawback
    is that important operations like `findAndModify` are absent. I am a bit
    uncomfortable with those NoSQL databases. Maybe try something more SQL
    like SQLite + sequelizejs.

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var express  = require('express'),
-    http     = require('http'),
+var http     = require('http'),
     socketio = require('socket.io'),
     winston  = require('winston'),
-    expressWinston = require('express-winston'),
     path     = require('path'),
     util     = require('util'),
     config   = require('./lib/config');
@@ -19,7 +17,7 @@ winston.add(winston.transports.Console, {
   level: config.logLevel
 });
 
-var app = express(),
+var app = require('./lib/express'),
     server = http.createServer(app),
     io = socketio.listen(server, {
       logger: {
@@ -30,41 +28,7 @@ var app = express(),
       }
     });
 
-// Configuration of Express.js
-app.configure('development', function() {
-  app.use(require('connect-livereload')({
-    port: process.env.LIVERELOAD_PORT
-  }));
-});
-app.configure(function() {
-  app.use(express.static(config.static));
-  app.use(express.json());
-  app.use(express.urlencoded());
-  app.use(express.methodOverride());
-
-  app.use(expressWinston.logger({
-    transports: [
-      new winston.transports.Console({
-        timestamp: true,
-        colorize: true
-      })
-    ]
-  }));
-  app.use(app.router);
-  app.use(expressWinston.errorLogger({
-    transports: [
-      new winston.transports.Console({
-        timestamp: true,
-        colorize: true
-      })
-    ]
-  }));
-});
-app.configure('development', function() {
-  app.use(express.errorHandler());
-});
-
-// HTML applications
+// Static files
 function serve(file) {
   return function(req, res) {
     res.sendfile(path.join(config.static, file));
@@ -74,6 +38,9 @@ app.get('/', function(req, res) { res.redirect('/display'); });
 app.get('/admin', serve('admin.html'));
 app.get('/display', serve('display.html'));
 app.get('/unassigned', serve('unassigned.html'));
+
+// Database
+var models = require('./lib/models');
 
 // API
 require('./lib/api/display')(io.of('/display'));
