eda89a7aa509ea2836eaf756974e9c3808df6c8a
Vincent Bernat <vbe@deezer.com>
2014-04-01 15:13:28 +0200

    receiver: use a fallback to cookies if localstorage is not available
    On some devices, localStorage may have been disabled. Fallback to
    cookies if this is the case.

--- app/scripts/receiver/localstorage.js
+++ app/scripts/receiver/localstorage.js
@@ -5,10 +5,48 @@ define('localstorage', (function(window, undefined) {
     return window.localStorage;
   }
 
-  console.warn('[Dashkiosk] local storage support is not available');
-  return {
-    setItem: function() {},
-    getItem: function() { return null; }
+  console.warn('[Dashkiosk] local storage support is not available, fallback to cookies');
+  function Storage(name) {
+    this.name = name;
+  }
+  Storage.prototype.write = function(value) {
+    var days = 365,
+        date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+
+    var expires = '; expires='+date.toGMTString();
+    window.document.cookie = this.name + '=' + value + expires + '; path=/';
   };
+
+  Storage.prototype.read = function() {
+    var nameEQ = this.name + '=',
+        ca = document.cookie.split(';'),
+        i, c;
+
+    for (i=0; i < ca.length; i++) {
+      c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1, c.length);
+      }
+
+      if (c.indexOf(nameEQ) === 0) {
+        return c.substring(nameEQ.length, c.length);
+      }
+    }
+    return null;
+  };
+
+  Storage.prototype.getItem = function(key) {
+    var data = JSON.parse(this.read() || '{}');
+    return data[key];
+  };
+
+  Storage.prototype.setItem = function(key, value) {
+    var data = JSON.parse(this.read() || '{}');
+    data[key] = value;
+    this.write(JSON.stringify(data));
+  };
+
+  return new Storage('dashkiosk');
 
 })(window));
