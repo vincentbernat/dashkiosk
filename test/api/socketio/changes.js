65510c1404b23002fd34e3f507630fbb82f69f23
Vincent Bernat <vincent@bernat.im>
2014-04-06 18:50:45 +0200

    socketio: complete "changes" API to get incremental changes
    And add more tests.

--- test/api/socketio/changes.js
+++ test/api/socketio/changes.js
@@ -32,7 +32,7 @@ describe('socket.io changes API', function() {
   describe('on connect', function() {
     it('should send an empty snapshot', function(done) {
       var client = ioclient.connect('http://127.0.0.1:5000/changes', options);
-      client.on('snapshot', function(data) {
+      client.once('snapshot', function(data) {
         data.should.eql({});
         done();
       });
@@ -53,44 +53,123 @@ describe('socket.io changes API', function() {
             }, function(err) { done(err); })
             .then(function() {
               var client = ioclient.connect('http://127.0.0.1:5000/changes', options);
-              client.on('snapshot', function(data) {
-                // We should have three groups
-                _.pluck(data, 'name').sort()
-                  .should.eql(['test group 1', 'test group 2', 'unassigned'].sort());
+              client.once('snapshot', function(data) {
+                try {
+                  // We should have three groups
+                  _.pluck(data, 'name').sort()
+                    .should.eql(['test group 1', 'test group 2', 'unassigned'].sort());
 
-                // Let's check test group 1
-                var group1 = _.find(data, function(g) { return g.name === 'test group 1'; });
-                group1.should.have.property('description', 'my first new group');
-                group1.should.have.property('displays');
-                group1.displays.should.eql({});
-                group1.should.have.property('dashboards');
-                group1.dashboards.length.should.equal(2);
-                group1.dashboards[0].should.have.property('url', 'http://www.example1.com');
-                group1.dashboards[1].should.have.property('url', 'http://www.example3.com');
+                  // Let's check test group 1
+                  var group1 = _.find(data, function(g) { return g.name === 'test group 1'; });
+                  group1.should.have.property('description', 'my first new group');
+                  group1.should.have.property('displays');
+                  group1.displays.should.eql({});
+                  group1.should.have.property('dashboards');
+                  group1.dashboards.length.should.equal(2);
+                  group1.dashboards[0].should.have.property('url', 'http://www.example1.com');
+                  group1.dashboards[1].should.have.property('url', 'http://www.example3.com');
 
-                // Let's check test group 2
-                var group2 = _.find(data, function(g) { return g.name === 'test group 2'; });
-                group2.should.have.property('description', 'my second new group');
-                group2.should.have.property('displays');
-                group2.displays.should.eql({});
-                group2.should.have.property('dashboards');
-                group2.dashboards.length.should.equal(2);
-                group2.dashboards[0].should.have.property('url', 'http://www.example2.com');
-                group2.dashboards[0].should.have.property('timeout', 30);
-                group2.dashboards[1].should.have.property('url', 'http://www.example4.com');
+                  // Let's check test group 2
+                  var group2 = _.find(data, function(g) { return g.name === 'test group 2'; });
+                  group2.should.have.property('description', 'my second new group');
+                  group2.should.have.property('displays');
+                  group2.displays.should.eql({});
+                  group2.should.have.property('dashboards');
+                  group2.dashboards.length.should.equal(2);
+                  group2.dashboards[0].should.have.property('url', 'http://www.example2.com');
+                  group2.dashboards[0].should.have.property('timeout', 30);
+                  group2.dashboards[1].should.have.property('url', 'http://www.example4.com');
 
-                // Let's check unassigned
-                var unassigned = _.find(data, function(g) { return g.name === 'unassigned'; });
-                unassigned.should.have.property('description', 'Default group for unassigned displays');
-                unassigned.should.have.property('displays');
-                _.keys(unassigned.displays).length.should.equal(2); // Two clients
-                unassigned.should.have.property('dashboards');
-                unassigned.dashboards.length.should.equal(1);
+                  // Let's check unassigned
+                  var unassigned = _.find(data, function(g) { return g.name === 'unassigned'; });
+                  unassigned.should.have.property('description', 'Default group for unassigned displays');
+                  unassigned.should.have.property('displays');
+                  _.keys(unassigned.displays).length.should.equal(2); // Two clients
+                  unassigned.should.have.property('dashboards');
+                  unassigned.dashboards.length.should.equal(1);
 
-                done();
+                  done();
+                } catch (err) {
+                  done(err);
+                }
               });
             }, function(err) { done(err); });
         }, function(err) { done(err); });
+    });
+  });
+
+  describe('on update', function() {
+    it('should send a group update when adding a dashboard', function(done) {
+      var g = new models.Group('test group'),
+          d = models.Display.register(); // Just to ensure that the unassigned group already exists
+      Promise.all([g.create(), d])
+        .spread(function(group, display) {
+          var client = ioclient.connect('http://127.0.0.1:5000/changes', options);
+          client.once('snapshot', function(data) {
+            // Got a snapshot, let's add a dashboard
+            client.once('group.updated', function(data) {
+              try {
+                data.name.should.equal('test group');
+                data.id.should.equal(group.toJSON().id);
+                data.should.have.property('dashboards');
+                data.dashboards.length.should.equal(1);
+                data.dashboards[0].url.should.equal('http://www.example3.com');
+                done();
+              } catch (err) {
+                done(err);
+              }
+            });
+            group.addDashboard('http://www.example3.com')
+              .catch(function(err) { done(err); });
+          });
+        })
+        .catch(function(err) { done(err); });
+    });
+
+    it('should send a group delete when deleting a group', function(done) {
+      var g = new models.Group('test group'),
+          d = models.Display.register(); // Just to ensure that the unassigned group already exists
+      Promise.all([g.create(), d])
+        .spread(function(group, display) {
+          var client = ioclient.connect('http://127.0.0.1:5000/changes', options);
+          client.once('snapshot', function(data) {
+            // Got a snapshot
+            client.once('group.deleted', function(data) {
+              try {
+                data.id.should.equal(group.toJSON().id);
+                done();
+              } catch (err) {
+                done(err);
+              }
+            });
+            group.delete()
+              .catch(function(err) { done(err); });
+          });
+        })
+        .catch(function(err) { done(err); });
+    });
+
+    it('should send a display update when updating a display property', function(done) {
+      var d = models.Display.register(); // Just to ensure that the unassigned group already exists
+      d
+        .then(function(display) {
+          var client = ioclient.connect('http://127.0.0.1:5000/changes', options);
+          client.once('snapshot', function(data) {
+            // Got a snapshot
+            client.once('display.updated', function(data) {
+              try {
+                data.description.should.equal('shiny client');
+                data.id.should.equal(display.toJSON().id);
+                done();
+              } catch (err) {
+                done(err);
+              }
+            });
+            display.update({ description: 'shiny client' })
+              .catch(function(err) { done(err); });
+          });
+        })
+        .catch(function(err) { done(err); });
     });
   });
 
