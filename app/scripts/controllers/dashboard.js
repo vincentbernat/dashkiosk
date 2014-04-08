188df1024e91aa62bb837adce29132b41526d64f
Vincent Bernat <vbe@deezer.com>
2014-04-08 18:30:03 +0200

    admin: allow to edit one dashboard detail

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -2,4 +2,34 @@ angular.module('dashkiosk.controllers')
   .controller('DashboardCtrl', function() {
     'use strict';
 
+  })
+  .controller('EditDashboardCtrl', function($scope) {
+    'use strict';
+
+    var realDashboard = $scope.$parent.dashboard,
+        copyDashboard = $scope.dashboard = angular.copy(realDashboard);
+    $scope.submit = function() {
+      var modified = _.omit(copyDashboard, function(v, k) {
+        if (k[0] === '$') {
+          return true;
+        }
+        if (v === realDashboard[k]) {
+          return true;
+        }
+        return false;
+      });
+      realDashboard
+        .$update(modified)
+        .then(function() {
+          $scope.$hide();
+        });
+    };
+
+    // Destroy the dashboard.
+    $scope.delete = function() {
+      realDashboard.$delete();
+      $scope.$hide();
+    };
+
   });
+
