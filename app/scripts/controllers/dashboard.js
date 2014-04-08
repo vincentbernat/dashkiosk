d8bd28b5b21eb3d1e727887a792934e6fcb58615
Vincent Bernat <vbe@deezer.com>
2014-04-08 18:55:09 +0200

    admin: allow creation of new dashboards

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -7,28 +7,42 @@ angular.module('dashkiosk.controllers')
     'use strict';
 
     var realDashboard = $scope.$parent.dashboard,
-        copyDashboard = $scope.dashboard = angular.copy(realDashboard);
+        copyDashboard = $scope.dashboard = angular.copy(realDashboard || {});
     $scope.submit = function() {
-      var modified = _.omit(copyDashboard, function(v, k) {
-        if (k[0] === '$') {
-          return true;
-        }
-        if (v === realDashboard[k]) {
-          return true;
-        }
-        return false;
-      });
-      realDashboard
-        .$update(modified)
-        .then(function() {
-          $scope.$hide();
+      if (!!realDashboard) {
+        // Modification of an existing dashboard
+        var modified = _.omit(copyDashboard, function(v, k) {
+          if (k[0] === '$') {
+            return true;
+          }
+          if (v === realDashboard[k]) {
+            return true;
+          }
+          return false;
         });
+        realDashboard
+          .$update(modified)
+          .then(function() {
+            $scope.$hide();
+          });
+      } else {
+        // Creation of a new dashboard
+        $scope.$parent.group.dashboards
+          .$add(copyDashboard)
+          .then(function() {
+            $scope.$hide();
+          });
+      }
     };
 
     // Destroy the dashboard.
     $scope.delete = function() {
       realDashboard.$delete();
       $scope.$hide();
+    };
+
+    $scope.isNew = function() {
+      return !realDashboard;
     };
 
   });
