68511caccecf11b5ae5a3101812c45a17d33b797
Vincent Bernat <vincent@bernat.im>
2014-04-09 21:39:25 +0200

    receiver: specify screen dimensions on OSD

--- app/scripts/receiver/osd.js
+++ app/scripts/receiver/osd.js
@@ -1,13 +1,31 @@
-define('osd', (function($, undefined) {
+define('osd', (function(window, $, undefined) {
   'use strict';
 
   return {
-    hide: function() { $('.osd').removeClass('show'); },
-    show: function(text) {
+    hide: function() {
       $('.osd')
-        .text(text)
+        .removeClass('show');
+    },
+    show: function(text) {
+      $('.osd.technical')
+        .html(
+          '<p>clientWidth/Height: ' +
+            (window.document.documentElement.clientWidth || '???') + '×' +
+            (window.document.documentElement.clientHeight || '???') +
+            '</p><p>' +
+            '<p>innerWidth/Height: ' +
+            (window.innerWidth || '???') + '×' +
+            (window.innerHeight || '???') +
+            '</p><p>' +
+            '<p>screenWidth/Height: ' +
+            (window.screen.width || '???') + '×' +
+            (window.screen.height || '???') +
+            '</p>');
+      $('.osd.text')
+        .text(text);
+      $('.osd')
         .addClass('show');
     }
   };
 
-})(Zepto));
+})(window, Zepto));
