dbfa79d959ec7129f1ffe8ceedbf814c5f8ce708
Vincent Bernat <vincent@bernat.im>
2014-04-09 23:14:51 +0200

    admin: viewport configuration / use of a modal instead of a popover

--- app/scripts/controllers/display.js
+++ app/scripts/controllers/display.js
@@ -2,4 +2,33 @@ angular.module('dashkiosk.controllers')
   .controller('DisplayCtrl', function() {
     'use strict';
 
+  })
+  .controller('EditDisplayCtrl', function($scope) {
+    'use strict';
+
+    var realDisplay = $scope.$parent.display,
+        copyDisplay = $scope.display = angular.copy(realDisplay);
+    $scope.submit = function() {
+      var modified = _.omit(copyDisplay, function(v, k) {
+        if (k[0] === '$') {
+          return true;
+        }
+        if (v === realDisplay[k]) {
+          return true;
+        }
+        return false;
+      });
+      realDisplay
+        .$update(modified)
+        .then(function() {
+          $scope.$hide();
+        });
+    };
+
+    // Destroy the display
+    $scope.delete = function() {
+      realDisplay.$delete();
+      $scope.$hide();
+    };
+
   });
--- app/scripts/directives/admin.js
+++ app/scripts/directives/admin.js
@@ -1,1 +1,27 @@
 angular.module('dashkiosk.directives', []);
+
+angular.module('dashkiosk.directives')
+  .directive('viewport', function() {
+    'use strict';
+
+    var revp = /^\s*\d+\s*([x×]\s*\d+\s*)?$/;
+    return {
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl) {
+        ctrl.$parsers.unshift(function(viewValue) {
+          if (!viewValue) {
+            ctrl.$setValidity('viewport', true);
+            return null;
+          } else if (revp.test(viewValue)) {
+            ctrl.$setValidity('viewport', true);
+            return viewValue
+              .replace(/\s+/g, '')
+              .replace(/×/, 'x');
+          } else {
+            ctrl.$setValidity('viewport', false);
+            return undefined;
+          }
+        });
+      }
+    };
+  });
