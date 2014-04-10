f7e02e42cb943992193be6ed70bc7794c127ac02
Vincent Bernat <vincent@bernat.im>
2014-04-10 23:06:29 +0200

    admin: ability to reload a whole group

--- app/scripts/controllers/group.js
+++ app/scripts/controllers/group.js
@@ -5,4 +5,21 @@ angular.module('dashkiosk.controllers')
     $scope.attachDisplay = function(name) {
       $scope.group.$attach(name);
     };
+
+    // Return true if any display in the group is connected
+    $scope.anyConnected = function() {
+      return _.any($scope.group.displays, function(d) {
+        return d.connected;
+      });
+    };
+
+    // Reload any connected display in the group
+    $scope.reloadAll = function() {
+      _.each($scope.group.displays, function(d) {
+        if (d.connected) {
+          d.$reload();
+        }
+      });
+    };
+
   });
