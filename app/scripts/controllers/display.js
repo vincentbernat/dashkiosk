1762be477689892e13efe5bc2b014f59279981fa
Vincent Bernat <vincent@bernat.im>
2014-04-12 15:54:50 +0200

    admin: use real-looking displays for display
    This takes more place but this is more eye-candy. Moreover, we can
    display the description. And we show on/off by displaying an image. And
    the same image for all members of a group.

--- app/scripts/controllers/display.js
+++ app/scripts/controllers/display.js
@@ -1,6 +1,25 @@
+/*jshint bitwise: false*/
 angular.module('dashkiosk.controllers')
-  .controller('DisplayCtrl', function() {
+  .controller('DisplayCtrl', function($scope) {
     'use strict';
+
+    // We choose an appropriate background offset for each group such
+    // that all displays of the same group seem to have the same image
+    // but each group have a different image. Moreover, we want that
+    // to change depending on the selected dashboard.
+    $scope.backgroundOffset = function() {
+      var g = $scope.display.group,
+          d = (_.find($scope.groups[g].dashboards,
+                      { active: true }) || {}).id || -1;
+      // Ok, from g and d, we want to have two offsets.
+      var mw = g + 1, mz = d + 1, i;
+      for (i = 0; i < 4; i++) {
+        mz = 36969 * (mz & 65535) + (mz >>> 16);
+        mw = 18000 * (mw & 65535) + (mw >>> 16);
+      }
+      var r = (mz << 16) + mw;
+      return '-' + ((r & 65535) % 100) + '% -' + ((r >>> 16) % 100) + '%';
+    };
 
   })
   .controller('EditDisplayCtrl', function($scope, $q) {
