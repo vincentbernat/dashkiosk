35081f8d8d2eb3a0ce0e071e43fb5813c6decf0b
Vincent Bernat <vincent@bernat.im>
2014-04-12 18:57:02 +0200

    unassigned: shuffle the images first
    I am a bit tired to always get the same image first. ;-)

--- app/scripts/unassigned.js
+++ app/scripts/unassigned.js
@@ -64,7 +64,27 @@
     window.setTimeout(clock, delay);
   }
 
+  function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
   $(window).on('load', function() {
+    $('.background').after(shuffle($('.photo')));
     rotate(); // Rotate photos
     clock(); // Display clock
   });
