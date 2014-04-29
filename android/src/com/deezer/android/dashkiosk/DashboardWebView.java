7dee5b5fd3d1ff7846ef656f01a2b9c646ade237
Vincent Bernat <vbe@deezer.com>
2014-04-29 17:36:06 +0200

    android: use the appropriate JSInterface interface

--- android/src/com/deezer/android/dashkiosk/DashboardWebView.java
+++ android/src/com/deezer/android/dashkiosk/DashboardWebView.java
@@ -28,7 +28,7 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.JavascriptInterface;
+import org.chromium.content.browser.JavascriptInterface;
 import org.xwalk.core.XWalkPreferences;
 import org.xwalk.core.XWalkView;
 
