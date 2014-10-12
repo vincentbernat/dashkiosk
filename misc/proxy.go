bb2cdf7659885b426c317d2ae3410fe11411ad56
Vincent Bernat <vincent@bernat.im>
2014-10-12 20:04:32 +0200

    proxy: provide a simple proxy "redirecting" to HTTPS
    This is not useful at all currently (unless you have a browser which
    isn't able to do TLS). Next step is to make this proxy present a client
    certificate (embedded browsers not able to do that are more common).

---
+++ misc/proxy.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/elazarl/goproxy"
+	"log"
+	"flag"
+	"strings"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	addr := flag.String("addr", ":8080", "proxy listen address")
+	https := flag.String("https", "", "comma-separated list of domains that should be queried with TLS")
+	flag.Parse()
+
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = *verbose
+	proxy.OnRequest().DoFunc(func (req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		host := strings.SplitN(req.URL.Host, ":", 2)[0]
+		for _, domain := range strings.Split(*https, ",") {
+			m, _ := filepath.Match(domain, host)
+			if m {
+				req.URL.Scheme = "https"
+				break
+			}
+		}
+		return req, nil
+	})
+	log.Fatal(http.ListenAndServe(*addr, proxy))
+}
