ace4bf6ce4627a48609e7e63244e34795074dc5c
Vincent Bernat <vincent@bernat.im>
2015-09-25 07:47:42 +0200

    receiver: use pure CSS animation for default loading animation
    SVG SMIL is going to be deprecated and as is, we cannot just use CSS to
    style it. Moreover, this move enables us to not enable the animation on
    slow browsers.

--- app/scripts/receiver.js
+++ app/scripts/receiver.js
@@ -5,9 +5,7 @@
 (function(window, undefined) {
   'use strict';
 
-  var socketio = require('./receiver/socketio'),
-      errors = require('./receiver/errors'),
-      supervisor = require('./receiver/supervisor'),
+  var errors = require('./receiver/errors'),
       console = require('./receiver/console'),
       benchmark = require('./receiver/benchmark'),
       document = window.document;
@@ -18,10 +16,10 @@
   document.addEventListener('DOMContentLoaded', function() {
     benchmark.done(function() {
       document.querySelector('.show').classList.add('loading');
-      supervisor.ready();
+      // supervisor.ready();
       // OK, ready, connect to socket.io
       console.log('[Dashkiosk] dashkiosk ready, connect to socket.io server');
-      socketio.connect();
+      // socketio.connect();
     });
   });
 
